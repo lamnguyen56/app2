97a23c60b9f36ac2ebc388847d0ccd0d7afaa0a9
lam <nguyenthualam1@gmai.com>
2023-04-04 16:26:38 +0700

    4/4/23

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -5,18 +5,32 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.MediaController
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var  navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        setupWithNavController(bottomNavigationView, navController)
+
+
 
 //        val homeFragment = homeFragment()
 //        val addingFragment = addingFragment()
